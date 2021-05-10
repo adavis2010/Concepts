d0d63000aa200c9682207ddaabecbff0a86fc438
adavis2010 <andreandavis2010@gmail.com>
2021-05-10 13:14:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+            /*int i = 0; 
+
+            /*while (i < 10) // WHILE LOOP
+            {
+                Console.WriteLine("i = {0}", i);
+
+                i++; // increment
+            }
+            */
+
+          // int i = 0, j = 1;
+
+            //while (i < 2) //Nested WHILE loop
+            //{
+                //Console.WriteLine("i = {0}", i);
+               // i++;
+
+                //while (j < 2)
+               // {
+                    //Console.WriteLine("j = {0}", j);
+                    //j++;
+                //}
+            //}
+            
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Value of i: {0}", i);
+                int j = i;
+
+                i++;
+
+                do
+                {
+                    Console.WriteLine("Value of j: {0}", j);
+                    j++;
+                } while (j < 2);
+
+            } while (i < 2);
+
+        
+    
+} } }
